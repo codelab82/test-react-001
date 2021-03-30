30c521c3f8cb332e4b90f2dd38f9dbfef2083d41
IreneCesarini <i.cesarini@key2.it>
2021-03-30 17:53:10 +0200

    Otp component + form invio sms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Otp from './Otp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Codelab82 (TM)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn react
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: '',
+      submitted: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.number);
+    this.setState({ number: '', submitted:true });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h5>
+            Send OTP message
+        </h5>
+        {
+          this.state.submitted ? <Otp/> :
+          <form onSubmit={this.handleSubmit}>
+            <label>Inserisci il tuo numero di cellulare:</label><br />
+            <input type="text" className="input-number" value={this.state.number} onChange={(e) => this.setState({ number: e.target.value })} />
+            <button className="otp-btn" type="submit">INVIAMI OTP</button><br /><br />
+          </form>
+        }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
