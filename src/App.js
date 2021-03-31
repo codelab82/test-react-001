644f33452afc403a4d24b16c6b35851fe5bede04
IreneCesarini <i.cesarini@key2.it>
2021-03-31 11:44:42 +0200

    modifiche

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
 import Otp from './Otp';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: '',
-      submitted: false
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const App = () => {
 
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state.number);
-    this.setState({ number: '', submitted:true });
-  }
-
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h5>
-            Send OTP message
-        </h5>
-        {
-          this.state.submitted ? <Otp/> :
-          <form onSubmit={this.handleSubmit}>
-            <label>Inserisci il tuo numero di cellulare:</label><br />
-            <input type="text" className="input-number" value={this.state.number} onChange={(e) => this.setState({ number: e.target.value })} />
-            <button className="otp-btn" type="submit">INVIAMI OTP</button><br /><br />
-          </form>
-        }
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Otp />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/Otp.js
+++ src/Otp.js
@@ -2,50 +2,29 @@ import React from 'react';
 
 const Otp = () => {
 
-    // const generateOtp = () => {
-    //     let digits = "0123456789";
-    //     let OTP = '';
-    //     for (let i = 0; i < 4; i++){
-    //         OTP += digits[Math.floor(Math.random() * 10)]
-    //     }
-    //     return OTP;
-    // }
-    // const sms = 'Il tuo codice. @web-otp.glitch.me #' + generateOtp()
-
-    if ('OTPCredential' in window) {
+    if ('OTPCredential' in window) { 
         window.addEventListener('DOMContentLoaded', e => {
-          const input = document.querySelector('input[autocomplete="one-time-code"]');
-          if (!input) return;
-          // Cancel the Web OTP API if the form is submitted manually.
           const ac = new AbortController();
-          const form = input.closest('form');
-          if (form) {
-            form.addEventListener('submit', e => {
-              // Cancel the Web OTP API.
-              ac.abort();
-            });
-          }
-          // Invoke the Web OTP API
           navigator.credentials.get({
             otp: { transport:['sms'] },
             signal: ac.signal
           }).then(otp => {
-            input.value = otp.code;
-            // Automatically submit the form when an OTP is obtained.
-            if (form) form.submit();
+            alert(otp.code)
           }).catch(err => {
-            console.log(err);
+            console.log(err)
           });
-        });
+        })
+      } else {
+        alert('WebOTP not supported!.')
       }
 
- return(
-    <form action="/verify-otp" method="POST">
-        <label>OTP (one time password):</label><br />
-        <input type="text" className="input-number" autoComplete="one-time-code" inputMode="numeric" required/>
-        <button className="otp-btn" type="submit">VERIFICA</button>
-    </form>
- )
+    return (
+        <form action="/verify-otp" method="POST">
+            <label>OTP (one time password):</label><br />
+            <input type="text" className="input-number" autoComplete="one-time-code" inputMode="numeric" required />
+            <button className="otp-btn" type="submit">VERIFICA</button>
+        </form>
+    )
 }
 
 export default Otp;
