8e94323d1c932d8d23efc0bcc54c59a05834dd49
codelab82 <codelab82@gmail.com>
2021-04-06 16:13:04 +0200

    nuova prova

--- src/components/PictureTaker/PictureTaker.js
+++ src/components/PictureTaker/PictureTaker.js
@@ -7,7 +7,7 @@ export default function PictureTaker() {
     const [targetDebug, setTargetDebug] = useState(null);
 
     const handleCapture = (target) => {
-        setTargetDebug(JSON.stringify(target))
+        /* setTargetDebug(JSON.stringify(target)) */
         if (target.files) {
           if (target.files.length !== 0) {
             const file = target.files[0];
@@ -23,7 +23,7 @@ export default function PictureTaker() {
         <input accept="image/*" id="icon-button-file" type="file" capture="environment" onChange={(e) => handleCapture(e.target)}/>
         <div>
             <label>{imgURL}</label>
-            <img alt="preview" src={imgURL} style={{"max-width":"240px"}} />
+            <img alt="preview" src={imgURL} style={{"maxWidth":"240px"}} />
         </div>
         <div>
             <div>{targetDebug}</div>
